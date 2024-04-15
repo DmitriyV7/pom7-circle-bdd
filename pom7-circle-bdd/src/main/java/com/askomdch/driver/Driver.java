6805a13415da465cc1bbd1d2e9e1b6553e930dba
DmitriyV7 <verkh.dmitriy@gmail.com>
2024-04-15 14:16:36 -0500

    added config file

--- pom7-circle-bdd/src/main/java/com/askomdch/driver/Driver.java
+++ pom7-circle-bdd/src/main/java/com/askomdch/driver/Driver.java
@@ -1,42 +1,55 @@
 package com.askomdch.driver;
-
-import com.askomdch.config.ConfigReader;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
 public class Driver {
+
     private static WebDriver driver;
+
     private Driver(){}
 
-    public static WebDriver getDriver(){
-
-        String browser = ConfigReader.getProperty("browser");
-
+    public static WebDriver getDriver(String browser){
         if (driver == null){
-            switch (browser){
-                case "chrome":
-                    driver = new ChromeDriver();
-                    break;
-                case "firefox":
-                    driver = new FirefoxDriver();
-                    break;
-                case "safari":
-                    driver = new SafariDriver();
-                    break;
-                case "edge":
-                    driver = new EdgeDriver();
-                    break;
+            try{
+                switch (browser){
+                    case "chrome":
+                        WebDriverManager.chromedriver().setup();
+                        ChromeOptions options = new ChromeOptions();
+                        options.addArguments("--headless"); // Run in headless mode
+                        options.addArguments("--no-sandbox"); // Bypass OS security model
+                        options.addArguments("--disable-gpu"); // Applicable to windows os only
+                        options.addArguments("--window-size=1920,1080");
+//                    options.addArguments("--start-maximized"); // Maximize the browser on start
+                        options.addArguments("--enable-automation");
+                        options.addArguments("--disable-infobars");
+                        options.addArguments("--disable-dev-shm-usage");
+                        driver = new ChromeDriver(options);
+                        break;
+                    case "firefox":
+                        driver = new FirefoxDriver();
+                        break;
+                    case "edge":
+                        driver  = new EdgeDriver();
+                        break;
+                    default:
+                        throw new RuntimeException("Invalid Browser type: " + browser);
+                }
+                driver.manage().window().maximize();
+                driver.manage().deleteAllCookies();
+                driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+            }catch (Exception e){
+                System.err.println("Error initializing WebDriver: " + e.getMessage());
             }
-            driver.manage().window().maximize();
-            driver.manage().deleteAllCookies();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
         }
+
+
         return driver;
     }
 
@@ -46,5 +59,4 @@ public class Driver {
             driver = null;
         }
     }
-
 }
