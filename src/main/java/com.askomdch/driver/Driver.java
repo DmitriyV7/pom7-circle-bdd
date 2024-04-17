8901e262c817abccffb64d3eef1521e913cd3452
DmitriyV7 <verkh.dmitriy@gmail.com>
2024-04-16 22:20:11 -0500

    changed pom file

--- src/main/java/com.askomdch/driver/Driver.java
+++ src/main/java/com.askomdch/driver/Driver.java
@@ -1,55 +1,41 @@
 package com.askomdch.driver;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.askomdch.config.ConfigReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
 public class Driver {
-
     private static WebDriver driver;
-
     private Driver(){}
 
-    public static WebDriver getDriver(String browser){
+    public static WebDriver getDriver(){
+
+        String browser = ConfigReader.getProperty("browser");
+
         if (driver == null){
-            try{
-                switch (browser){
-                    case "chrome":
-                        WebDriverManager.chromedriver().setup();
-                        ChromeOptions options = new ChromeOptions();
-                        options.addArguments("--headless"); // Run in headless mode
-                        options.addArguments("--no-sandbox"); // Bypass OS security model
-                        options.addArguments("--disable-gpu"); // Applicable to windows os only
-                        options.addArguments("--window-size=1920,1080");
-//                    options.addArguments("--start-maximized"); // Maximize the browser on start
-                        options.addArguments("--enable-automation");
-                        options.addArguments("--disable-infobars");
-                        options.addArguments("--disable-dev-shm-usage");
-                        driver = new ChromeDriver(options);
-                        break;
-                    case "firefox":
-                        driver = new FirefoxDriver();
-                        break;
-                    case "edge":
-                        driver  = new EdgeDriver();
-                        break;
-                    default:
-                        throw new RuntimeException("Invalid Browser type: " + browser);
-                }
-                driver.manage().window().maximize();
-                driver.manage().deleteAllCookies();
-                driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
-                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-            }catch (Exception e){
-                System.err.println("Error initializing WebDriver: " + e.getMessage());
+            switch (browser){
+                case "chrome":
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    driver = new FirefoxDriver();
+                    break;
+                case "safari":
+                    driver = new SafariDriver();
+                    break;
+                case "edge":
+                    driver = new EdgeDriver();
+                    break;
             }
+            driver.manage().window().maximize();
+            driver.manage().deleteAllCookies();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
         }
-
-
         return driver;
     }
 
@@ -59,4 +45,5 @@ public class Driver {
             driver = null;
         }
     }
+
 }
