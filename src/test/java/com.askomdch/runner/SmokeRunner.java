526d6ffdf030c24e9047243e58ba7d402a3fdb7e
DmitriyV7 <verkh.dmitriy@gmail.com>
2024-04-17 12:17:17 -0500

    changed pom

--- src/test/java/com.askomdch/runner/SmokeRunner.java
+++ src/test/java/com.askomdch/runner/SmokeRunner.java
@@ -8,9 +8,9 @@ import org.junit.runner.RunWith;
         glue = {"com/askomdch/step_definitions", "com/askomdch/hooks"},
         dryRun = false,
         tags = "@smoke",
-        plugin={"pretty", "junit:target/JUNITReport/smokeReport.xml",
-                "json:target/JSONReport/smokeReport.json",
-                "html:target/HtmlReport/smokeReport.html"}
+        plugin={"pretty", "junit:target/surefire-reports/smokeReport.xml",
+                "json:target/surefire-reports/smokeReport.json",
+                "html:target/surefire-reports/smokeReport.html"}
 )
 public class SmokeRunner {
 
