3561f65cc9aa1c153bc73b2284be7f8d91041654
DmitriyV7 <verkh.dmitriy@gmail.com>
2024-04-18 15:27:50 -0500

    pom updated

--- src/main/java/com.askomdch/driver/Driver.java
+++ src/main/java/com.askomdch/driver/Driver.java
@@ -23,8 +23,9 @@ public class Driver {
         if (driver == null){
             switch (browser){
                 case "chrome":
-                    WebDriverManager.chromedriver().clearDriverCache().setup();
-                    WebDriverManager.chromedriver().clearResolutionCache().setup();
+//                    WebDriverManager.chromedriver().clearDriverCache().setup();
+//                    WebDriverManager.chromedriver().clearResolutionCache().setup();
+                    WebDriverManager.chromedriver().setup();
                     ChromeOptions chromeOptions = new ChromeOptions();
 //                    chromeOptions.addArguments("--headless");
                     chromeOptions.addArguments("--window-size=1920,1080");
